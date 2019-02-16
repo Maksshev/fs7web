8fd96fa18d82291511cb2aac18670a6ed0d7d0e8
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-02-16 13:12:25 +0200

    metadata added #3 minor fixed

--- db/metatada/metadata.sql
+++ db/metatada/metadata.sql
@@ -1,13 +1,13 @@
 CREATE TABLE history (
-    id INTEGER DEFAULT nextval('history_id_seq1'::regclass) PRIMARY KEY NOT NULL,
+    id serial,
     a INTEGER,
     op VARCHAR,
     b INTEGER,
     r INTEGER,
-    at TIMESTAMP DEFAULT now()
+    at TIMESTAMP DEFAULT now(),
     user_id INTEGER
 );
 CREATE TABLE users (
-    id INTEGER DEFAULT nextval('users_id_seq'::regclass) PRIMARY KEY NOT NULL,
+    id serial,
     name VARCHAR(32)
 );
