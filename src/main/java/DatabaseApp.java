270c5d8aed3e05c54046440b5ebdcb754827e994
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-02-16 11:14:17 +0200

    db INSERT statement

--- src/main/java/DatabaseApp.java
+++ src/main/java/DatabaseApp.java
@@ -7,9 +7,13 @@ import java.sql.SQLException;
 public class DatabaseApp {
     public static void main(String[] args) throws SQLException {
         Connection conn = new DbConnection().connection();
-        String sql = "INSERT INTO authors (\"firstName\")  VALUES (?)";
+        //String sql = "INSERT INTO authors (\"firstName\")  VALUES (?)";
+        String sql = "INSERT INTO history (a, op, b, r) VALUES (?, ?, ?, ?)";
         PreparedStatement stm = conn.prepareStatement(sql);
-        stm.setString(1, "DENIS");
+        stm.setInt(1, 11);
+        stm.setString(2, "+");
+        stm.setInt(3, 22);
+        stm.setInt(4, 33);
         stm.execute();
     }
 }
